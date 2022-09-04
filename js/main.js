cd6836f9ffd36ec9c81eb6e75723c77263c303d7
Diego Chagas <diegofchagas@gmail.com>
2022-09-04 15:32:49 -0300

    subindo projeto portifolio

---
+++ js/main.js
@@ -0,0 +1,129 @@
+
+
+const showMenu=(toggleId,navId)=>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+    if(toggle && nav){
+        toggle.addEventListener('click',()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+
+showMenu('nav-toggle', 'nav-menu')
+
+const navLink=document.querySelectorAll('.nav_link')
+function linkAction(){
+ 
+    navLink.forEach(n=>n.classList.remove('active'))
+    this.classList.add('active')
+ 
+    const navMenu=document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+}
+
+navLink.forEach(n=>n.addEventListener('click',linkAction))
+
+
+const sr = ScrollReveal({
+ origin:'top',
+ distance: '80px',
+ duration: 2000,
+ reset:true
+
+})
+
+sr.reveal('.home_title',{})
+sr.reveal('.button',{delay:200})
+sr.reveal('.home_img',{delay:400})
+sr.reveal('.home_social-icon',{interval:200})
+
+
+sr.reveal('.home_img',{})
+sr.reveal('.about_subtitle',{delay:200})
+sr.reveal('.about_text',{delay:400})
+
+
+sr.reveal('.skills_subtitle',{})
+sr.reveal('.skills_text',{delay:200})
+sr.reveal('.skills_data',{interval:200})
+sr.reveal('.skills_img',{delay:400})
+
+
+sr.reveal('.work_img',{interval:200})
+
+
+sr.reveal('.contact_input',{interval:200})
+
+
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+ 
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+
+
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'bx-sun'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-moon' : 'bx-sun'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
+
+const btnForm = document.querySelector(".contact_button") 
+ btnForm.addEventListener("click", function(){
+
+    setTimeout(() => {
+        alert("Email Enviado Obrigado")
+    }, 700);
+    
+ })
+
+
+ 
